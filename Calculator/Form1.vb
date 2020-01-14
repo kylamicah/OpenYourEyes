cb9bd738c939fe47ac730e5947a7faeb0e54f689
Kylamicah Gallarda <kylagallarda031@gmail.com>
2020-01-14 19:52:50 +0800

    CalculatorUI

---
+++ Calculator/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Const V As Integer = 1
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Label1.Text = V
+    End Sub
+End Class
