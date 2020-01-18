37c3b7dbe0fe2803c0bf99ed1dab2907fcc3fc33
Kylamicah Gallarda <kylagallarda031@gmail.com>
2020-01-18 20:38:46 +0800

    working buttons

--- Calculator/Form1.vb
+++ Calculator/Form1.vb
@@ -1,7 +1,52 @@
 ﻿Public Class Form1
-    Private Const V As Integer = 1
 
-    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
-        Label1.Text = V
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If Label1.Text <> "0" Then
+            Label1.Text += "0"
+        End If
+
     End Sub
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Label1.Text += "1"
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Label1.Text += "2"
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Label1.Text += "3"
+
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Label1.Text += "4"
+
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Label1.Text += "5"
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Label1.Text += "6"
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        Label1.Text += "7"
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        Label1.Text += "8"
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        Label1.Text += "9"
+    End Sub
+
 End Class
+
+
